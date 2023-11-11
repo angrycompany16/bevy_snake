84127108ddb0eb6bd8e7562d8d0cba804c3c5a9a
angrycompany16 <dpersson16@gmail.com>
2023-11-12 00:31:57 +0100

    i love feet

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ const SNAKE_HEAD_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 const SNAKE_BODY_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
 const FOOD_COLOR: Color = Color::rgb(0.9, 0.2, 0.2);
 
-const TICK_RATE: f32 = 8.;
+const TICK_RATE: f32 = 8.0;
 
 fn main() {
     App::new()
@@ -30,11 +30,12 @@ fn main() {
         .add_plugins(WorldInspectorPlugin::new())
         .insert_resource(
             SnakeUpdateTimer(
-                Timer::from_seconds(1. / TICK_RATE, TimerMode::Repeating)
+                Timer::from_seconds(1.0 / TICK_RATE, TimerMode::Repeating)
             )
         )
         .add_systems(Startup, (setup, spawn_food))
-        .add_systems(Update, (move_snake, control_snake, tick_timer))
+        .add_systems(Update, (
+            move_snake, control_snake, tick_timer, check_food_collision, extend_snake_system, spawn_food))
         .add_event::<EatFoodEvent>()
         .run();
 }
@@ -85,7 +86,7 @@ impl SnakeBodyPartBundle {
             spritebundle: SpriteBundle {
                 transform: Transform {
                     translation: convert_coordinates(spawn_pos).extend(0.0),
-                    scale: vec3(CELL_DIMENSIONS.x, CELL_DIMENSIONS.y, 1.),
+                    scale: vec3(CELL_DIMENSIONS.x, CELL_DIMENSIONS.y, 1.0),
                     ..default()
                 },
                 sprite: Sprite {
@@ -105,13 +106,14 @@ impl SnakeFoodBundle {
             spritebundle: SpriteBundle {
                 transform: Transform {
                     translation: convert_coordinates(spawn_pos).extend(0.0),
-                    scale: vec3(CELL_DIMENSIONS.x, CELL_DIMENSIONS.y, 1.),
+                    scale: vec3(CELL_DIMENSIONS.x, CELL_DIMENSIONS.y, 1.0),
                     ..default()
                 },
                 sprite: Sprite {
                     color: FOOD_COLOR,
                     ..default()
                 },
+                visibility: Visibility::Visible,
                 ..default()
             },
             snake_food: SnakeFood {
@@ -121,9 +123,7 @@ impl SnakeFoodBundle {
     }
 }
 
-fn setup(
-    mut commands: Commands
-) {
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 
     commands.insert_resource(ClearColor(BG_COLOR));
@@ -143,7 +143,7 @@ fn setup(
         },
         Snake {
             direction: Direction::Right,
-            positions: vec![START_POS, START_POS - vec2(1., 0.)]
+            positions: vec![START_POS, START_POS - vec2(1.0, 0.0)]
         },
         Name::new("Snake positions")
     )).id();
@@ -159,6 +159,16 @@ fn setup(
     )).id();
 
     commands.entity(snake).add_child(snake_head).add_child(snake_tail);
+    
+    let mut rng = rand::thread_rng();
+    
+    commands.spawn((
+        SnakeFoodBundle::new(vec2(
+            (rng.gen::<f32>() * GRID_DIMENSIONS.x).round(), 
+            (rng.gen::<f32>() * GRID_DIMENSIONS.y).round()
+        )),
+        Name::new("food")
+    ));
 }
 
 fn move_snake(
@@ -172,15 +182,15 @@ fn move_snake(
     ) = snake_query.single_mut();
     
     if timer.just_finished() {
-        for i in (snake.positions.len() - 1)..=1 {
-            snake.positions[i] = snake.positions[i - 1];            
+        for i in (1..snake.positions.len()).rev() {
+            snake.positions[i] = snake.positions[i - 1];
         }
 
         match snake.direction {
-            Direction::Left => { snake.positions[0].x -= 1.; },
-            Direction::Right => { snake.positions[0].x += 1.; },
-            Direction::Up => { snake.positions[0].y -= 1.; },
-            Direction::Down => { snake.positions[0].y += 1.; }
+            Direction::Left => { snake.positions[0].x -= 1.0; },
+            Direction::Right => { snake.positions[0].x += 1.0; },
+            Direction::Up => { snake.positions[0].y -= 1.0; },
+            Direction::Down => { snake.positions[0].y += 1.0; }
         }
 
         if snake.positions[0].x >= GRID_DIMENSIONS.x {
@@ -197,7 +207,7 @@ fn move_snake(
     
         for (i, child) in children.iter().enumerate() {
             if let Ok(mut transform) = snake_part_query.get_mut(*child) {
-                transform.translation = convert_coordinates(snake.positions[i]).extend(0.);
+                transform.translation = convert_coordinates(snake.positions[i]).extend(0.0);
             }
         }
     }
@@ -222,31 +232,38 @@ fn check_food_collision(
     }
 }
 
-fn spawn_food(mut commands: Commands) {
-    let mut rng = rand::thread_rng();
-
-    commands.spawn(SnakeFoodBundle::new(vec2(
-        (rng.gen::<f32>() * GRID_DIMENSIONS.x).round(), 
-        (rng.gen::<f32>() * GRID_DIMENSIONS.y).round()
-    )));
+fn spawn_food(mut commands: Commands, mut ev_eat_food: EventReader<EatFoodEvent>) {
+    for _ in ev_eat_food.read() {
+        let mut rng = rand::thread_rng();
+    
+        commands.spawn(SnakeFoodBundle::new(vec2(
+            (rng.gen::<f32>() * GRID_DIMENSIONS.x - 1.0).round(), 
+            (rng.gen::<f32>() * GRID_DIMENSIONS.y - 1.0).round()
+        )));
+    }
 }
 
 fn extend_snake_system(
     mut commands: Commands, 
-    mut snake_query: Query<Entity, With<Snake>>, 
+    mut snake_query: Query<(Entity, &mut Snake)>, 
     mut ev_eat_food: EventReader<EatFoodEvent>
 ) {
-    for ev in ev_eat_food {
+    for _ in ev_eat_food.read() {
+        let (snake_entity, mut snake) = snake_query.single_mut();
+        
+        let tail_pos = *snake.positions.last().expect("snake tail position");
+        
+        snake.positions.push(tail_pos);
+
         let new_snake_part = commands.spawn(
-            SnakeBodyPartBundle::new(vec2(0.0, 0.0))
+            SnakeBodyPartBundle::new(tail_pos)
         ).id();
-    
-        let snake = snake_query.single_mut();
-        let mut snake_entity = commands.entity(snake);
+        
+        let mut snake_entity = commands.entity(snake_entity);
     
         snake_entity.add_child(new_snake_part);
-    }
 
+    }
 }
 
 fn control_snake(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Snake>) {
